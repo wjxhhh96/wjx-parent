39a8912bca16bd58eea2ee3d1d03736e80160042
wjxhhh96 <342397547@qq.com>
2021-11-12 14:24:05 +0800

    [feature]init security demo

---
+++ security-oauth2-demo/src/main/java/com/security/SecurityApplication.java
@@ -0,0 +1,22 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * <b>SecurityApplication</b>
+ * </p>
+ *
+ * @author Wjx
+ * @Description: TODO
+ * @since 2021/11/12
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class,args);
+    }
+
+}
